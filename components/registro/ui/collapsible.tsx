67b4236a6e580c9bc18466868e84b1568522a712
VI-Alumno <VI-Alumno@duoc.acad>
2026-04-30 09:28:16 -0400

    Se modificaron las rutas de archivos

--- components/registro/ui/collapsible.tsx
+++ components/registro/ui/collapsible.tsx
@@ -1,9 +1,9 @@
 import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
+import { ThemedText } from '@/components/registro/themed-text';
+import { ThemedView } from '@/components/registro/themed-view';
+import { IconSymbol } from '@/components/registro/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
@@ -24,7 +24,6 @@ export function Collapsible({ children, title }: PropsWithChildren & { title: st
           color={theme === 'light' ? Colors.light.icon : Colors.dark.icon}
           style={{ transform: [{ rotate: isOpen ? '90deg' : '0deg' }] }}
         />
-
         <ThemedText type="defaultSemiBold">{title}</ThemedText>
       </TouchableOpacity>
       {isOpen && <ThemedView style={styles.content}>{children}</ThemedView>}
