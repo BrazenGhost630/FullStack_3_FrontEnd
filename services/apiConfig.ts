dba2aba9857c2feb3d9bb8f1623c974e67c74a7a
Pablo <pp-pjimenez@zonar.cl>
2026-06-09 20:50:34 -0400

    se añadieron terminos y condiciones del servicio

--- services/apiConfig.ts
+++ services/apiConfig.ts
@@ -4,7 +4,7 @@ import { Platform } from 'react-native';
 // Reemplaza 'TU_IP_LOCAL' con la dirección IP de tu computadora en tu red Wi-Fi.
 // La IP '10.0.2.2' solo funciona para emuladores de Android, no para teléfonos físicos.
 // Puedes encontrar tu IP en Windows con 'ipconfig' o en Mac/Linux con 'ifconfig'.
-const MI_COMPUTADORA_IP = '192.168.1.9'; // ¡ÚNICO LUGAR PARA CAMBIAR LA IP!
+const MI_COMPUTADORA_IP = '10.70.10.155'; // ¡ÚNICO LUGAR PARA CAMBIAR LA IP!
 
 // URL para el servicio de autenticación y prendas (puerto 8080)
 const AUTH_API_URL = process.env.EXPO_PUBLIC_API_URL || (Platform.OS === "web" ? "http://localhost:8080/api" : `http://${MI_COMPUTADORA_IP}:8080/api`);
